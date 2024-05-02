668ac82b8ef54c3e02d2ff838bec9344a8ba56ba
Goecha <goecha.ksh@gmail.com>
2024-05-02 20:43:00 +0530

    Accordion Using React JS

---
+++ src/components/data.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+    id: 1,
+    question: "What is the capital of France?",
+    answer: "Paris"
+  },
+  {
+    id: 2,
+    question: "Who wrote 'Romeo and Juliet'?",
+    answer: "William Shakespeare"
+  },
+  {
+    id: 3,
+    question: "What is the chemical symbol for water?",
+    answer: "H2O"
+  }
+];
+
+export default data
